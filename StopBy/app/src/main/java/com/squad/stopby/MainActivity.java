2323298c51c4ffa5cea160f44bffe6d0ddce049f
Rachel Redder <rredder@buffalo.edu>
2018-03-07 18:52:49 -0500

    Firebase is connected to app

--- StopBy/app/src/main/java/com/squad/stopby/MainActivity.java
+++ StopBy/app/src/main/java/com/squad/stopby/MainActivity.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText username = (EditText) findViewById(R.id.username);
+        EditText password = (EditText) findViewById(R.id.password);
+        Button login = (Button) findViewById(R.id.login);
+
+
+        // get instance of firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReferenceFromUrl("https://stopby-196918.firebaseio.com/");
+
+        myRef.setValue("Hello, World!");
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //myRef.setValue("Hello World");
+            }
+        });
     }
 
     //Called when user logs in
