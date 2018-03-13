8b68df5c00a96e8e8737d9008bdbdc430fbfb281
Rachel Redder <rredder@buffalo.edu>
2018-03-12 20:10:29 -0400

    Added code structure for pushing user post and location to database

--- StopBy/app/src/main/java/com/squad/stopby/Post.java
+++ StopBy/app/src/main/java/com/squad/stopby/Post.java
@@ -1,6 +1,7 @@
 package com.squad.stopby;
 
 import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,38 +10,68 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Post extends AppCompatActivity {
     private RadioGroup radioGroup;
     private RadioButton hangoutButton;
     private RadioButton studyButton;
-    private EditText timeField;
     private EditText placeField;
     private Button postButton;
+    private EditText timeField;
+
+    private FusedLocationProviderClient mFusedLocationClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+        final EditText message = findViewById(R.id.timeField);
 
-        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
-        hangoutButton = (RadioButton) findViewById(R.id.hangoutButton);
-        studyButton = (RadioButton) findViewById(R.id.studyButton);
-        timeField = (EditText) findViewById(R.id.timeField);
-        placeField = (EditText) findViewById(R.id.placeField);
-        postButton = (Button) findViewById(R.id.postButton);
+        //TODO this section will get users location, need to convert this to something storable to put in database
+        /*
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+
+                        }
+                    }
+                });
+*/
+
+        //Instance of Firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference databaseReference = database.getReferenceFromUrl("https://stopby-196918.firebaseio.com/");
+
+        Button postButton =  findViewById(R.id.postButton);
 
         postButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //transfer data from Post to MapsActivity
-                String event = getValueOfClickedButton();
-                String time = timeField.getText().toString();
-                String place = placeField.getText().toString();
-                post2Map(event, time, place);
+                Intent goToActivePost = new Intent(Post.this, ActivePost.class);
+                startActivity(goToActivePost);
+
+                String msg = message.getText().toString();
+                //TODO pass Username and location data
+                //send username and post message to the database
+                databaseReference.child("Location").child("User3").child("Post").setValue(msg);
+                databaseReference.child("Location").child("User3").child("Coordinates").setValue(12345);
+
             }
         });
     }
 
+    /*
     public void post2Map(String event, String time, String place) {
         Intent intent= new Intent(this, MapsActivity.class);
         Bundle extras = new Bundle();
@@ -49,12 +80,19 @@ public class Post extends AppCompatActivity {
         extras.putString("place", place);
         intent.putExtras(extras);
         startActivity(intent);
-    }
+    }*/
 
     //To get the value of the clicked radio button
     public String getValueOfClickedButton() {
         int clickedButtonId = radioGroup.getCheckedRadioButtonId();
-        RadioButton clickedButton = (RadioButton) findViewById(clickedButtonId);
+        RadioButton clickedButton = findViewById(clickedButtonId);
         return clickedButton.getText().toString();
+    }
+
+
+    public void pushLocation(Double location, String username, String post, DatabaseReference ref)
+    {
+
+        ref.child("Location").child(username).setValue(post);
     }
 }
