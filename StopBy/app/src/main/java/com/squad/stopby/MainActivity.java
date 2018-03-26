0c74be2c96507487bc54a8dcc63bbfaaa55ef382
Iman Abdul-Rashed <ihabdulr@buffalo.edu>
2018-03-26 13:32:06 -0400

    Merge branch 'Kim' of https://github.com/rmredder/StopBy into Iman
    Signed-off-by: Iman Abdul-Rashed <ihabdulr@buffalo.edu>
    
    # Conflicts:
    #	StopBy/app/src/main/java/com/squad/stopby/MainActivity.java
    #	StopBy/app/src/main/java/com/squad/stopby/Profile.java

--- StopBy/app/src/main/java/com/squad/stopby/MainActivity.java
+++ StopBy/app/src/main/java/com/squad/stopby/MainActivity.java
@@ -3,48 +3,85 @@ package com.squad.stopby;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.view.*;
+import android.view.Menu;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+
+    private Button main_postBtn;
+    private Button main_searchBtn;
+    private Button main_profileBtn;
+
+    private android.support.v7.widget.Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText username = findViewById(R.id.username);
-        EditText password = findViewById(R.id.password);
-        Button login = findViewById(R.id.login);
+        //Authentication
+        mAuth = FirebaseAuth.getInstance();
 
+        main_postBtn = (Button) findViewById(R.id.main_postBtn);
+        main_searchBtn = (Button) findViewById(R.id.main_searchBtn);
+        main_profileBtn = (Button) findViewById(R.id.main_profileBtn);
 
-        // get instance of firebase
-        //FirebaseDatabase database = FirebaseDatabase.getInstance();
-        //final DatabaseReference myRef = database.getReferenceFromUrl("https://stopby-196918.firebaseio.com/");
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.main_menu);
+        setSupportActionBar(toolbar);
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                login(view);
-            }
-        });
+//        main_postBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                mDatabaseReference.push().setValue("hello world");
+//            }
+//        });
     }
 
-    public void login(View view){
-        Intent goToMenu = new Intent(this, Menu.class);
-        startActivity(goToMenu);
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser ==  null) {
+            Intent intent = new Intent(this, StartupActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Welcome back brodie", Toast.LENGTH_SHORT).show();
+        }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.main_signout) {
+            FirebaseAuth.getInstance().signOut();
+            //update UI
+            Intent intent = new Intent(this, StartupActivity.class);
+            startActivity(intent);
+        }
+        return true;
+    }
 
     public void findUsers(DatabaseReference ref)
     {
-       // ref.addListenerForSingleValueEvent(ValueEventListener v);
+        // ref.addListenerForSingleValueEvent(ValueEventListener v);
     }
 
 
--- StopBy/app/src/main/java/com/squad/stopby/Profile.java
+++ StopBy/app/src/main/java/com/squad/stopby/Profile.java
@@ -12,21 +12,17 @@ public class Profile {
     private String username;
     private String email;
     private String password;
-<<<<<<< HEAD
-
-    public Profile(String username, String email, String password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-=======
     private String userInfo;
+
+
+
 
     public Profile(String username, String email, String password, String userInfo) {
         this.username = username;
         this.email = email;
         this.password = password;
         this.userInfo = userInfo;
->>>>>>> 1dbe265afb7e0ca77ead138fcdb35406c3e85b80
+
     }
 
     public String getUsername() {
@@ -52,10 +48,7 @@ public class Profile {
     public void setPassword(String password) {
         this.password = password;
     }
-<<<<<<< HEAD
-}
 
-=======
 
     public String getUserInfo() {
         return userInfo;
@@ -65,4 +58,3 @@ public class Profile {
         this.userInfo = userInfo;
     }
 }
->>>>>>> 1dbe265afb7e0ca77ead138fcdb35406c3e85b80
