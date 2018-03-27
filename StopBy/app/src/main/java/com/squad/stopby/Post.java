ce99c7931a46e392cbe4e30d6354030d44af0404
Rachel Redder <rredder@buffalo.edu>
2018-03-27 19:43:53 -0400

    added location listener to post activity

--- StopBy/app/src/main/java/com/squad/stopby/Post.java
+++ StopBy/app/src/main/java/com/squad/stopby/Post.java
@@ -1,6 +1,12 @@
 package com.squad.stopby;
 
-import android.content.Intent;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,10 +31,29 @@ public class Post extends AppCompatActivity {
     private EditText post_messageField;
     private Button postBtn;
 
+    LocationManager locationManager;
+    LocationListener locationListener;
+
     private String userLatitude;
     private String userLongitude;
 
     private String username;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        // If request is cancelled, the result arrays are empty.
+        if(requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    //TODO edit min time and min distance for battery efficiency purposes
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                }
+            }
+        }
+        return;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +64,44 @@ public class Post extends AppCompatActivity {
         postBtn = (Button) findViewById(R.id.postbtn);
 
         //current user's location coordinate
-        userLatitude = getIntent().getStringExtra("Latitude");
-        userLongitude = getIntent().getStringExtra("Longitude");
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.e("Location: ", location.toString());
+                userLatitude = Double.toString(location.getLatitude());
+                userLongitude = Double.toString(location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        //Check for permission to get users location
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }else{
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            Location firstLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            userLatitude = Double.toString(firstLocation.getLatitude());
+            userLongitude = Double.toString(firstLocation.getLongitude());
+
+        }
+
 
         //Instance of Firebase
         db = new Database();
@@ -69,7 +130,6 @@ public class Post extends AppCompatActivity {
                 String message = post_messageField.getText().toString();
 
                 //send username and post message to the database
-                //TODO need to pass correct username
                 LocationDB locationDB = new LocationDB(username, message, userLatitude, userLongitude);
                 locationDB.pushToDatabase(db.getDatabaseReference());
 
